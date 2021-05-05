0eb4648f1c57d0b1544a5c4ba4f16b4392fa8823
huangrh <824954745@qq.com>
2021-05-05 18:30:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/Test.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @Auther: huangrh
+ * @Date: 2021/5/5 - 05 - 05 - 18:28
+ * @Description: com.atguigu
+ * @version: 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
